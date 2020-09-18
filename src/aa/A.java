a6a6652906e9d5cb44afe5030ab77aa8e5b47119
1943404414 <1943404414@qq.com>
2020-09-18 19:43:20 +0800

    提交

---
+++ src/aa/A.java
@@ -0,0 +1,7 @@
+package aa;
+
+public class A {
+	public static void tt() {
+		System.out.println("±¯ÉËµÄÉÙÄê");
+	}
+}
---
+++ src/aa/B.java
@@ -0,0 +1,7 @@
+package aa;
+
+public class B {
+	public static void main(String[] args) {
+		A.tt();
+	}
+}
